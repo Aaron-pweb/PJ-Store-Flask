af081165f26cdc4b49eaed67d9a080f31d524bab
Aaron Teshale <aaronteshale138@gmail.com>
2026-04-15 09:15:10 +0300

    updating the database server to Postgresql

--- app/__init__.py
+++ app/__init__.py
@@ -14,7 +14,7 @@ load_dotenv()
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
     app.secret_key = os.getenv("APP_SECRET_KEY", "dev_key")
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://aaron:your_secure_password@localhost:5432/pj_store'
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.init_app(app)
