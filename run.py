4c0cf6365afa80a33b7cdcf1cf4d2b51f37e763b
Aaron Teshale <aaronteshale138@gmail.com>
2026-08-21 09:56:03 +0300

    feat: docker service database integration

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 from app import create_app
+import os
 
 app = create_app()
+# Fetches the URL from the docker-compose environment, handling the connection routing
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or 'sqlite:///default.db'
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
